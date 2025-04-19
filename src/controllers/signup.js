3083dca9121d4a6d09731a587e4877a39a125cf8
nenio15 <2857yoon@gmail.com>
2025-04-19 12:09:05 +0900

    [Fix] db query 이전 db connection

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -1,11 +1,9 @@
 import dotenv from 'dotenv';
+dotenv.config();
 import bcrypt from 'bcrypt';
 import { pool } from '../config/db.js';
 
-dotenv.config();
-
 // 회원가입
-
 export const signup = async (req, res) => {
   const { email, password, name, userType } = req.body;
 
@@ -13,22 +11,24 @@ export const signup = async (req, res) => {
     return res.status(400).json({ message: '필수 항목 누락' });
   }
 
-  try{
-  const [exist] = await pool.query('SELECT * FROM User WHERE email = ?', [email])
-  if (exist.length > 0) {
-    return res.status(400).json({ message: '이미 등록된 이메일입니다' });
-  }
+  try {
+    const connection = await pool.getConnection();
 
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const date = new Date(+new Date() + 3240 * 10000).toISOString().split("T")[0];
-  
-  // 관리자 승인 미구현
-  const [result] = await pool.query(
-      'INSERT INTO User (email, password, name, userType, isApproved, createdAt) VALUES (?, ?, ?, ?, ?, ?)',
-      [email, hashedPassword, name, userType, true, date] 
-  );
+    const [exist] = await pool.query('SELECT * FROM User WHERE email = ?', [email])
+    if (exist.length > 0) {
+      return res.status(400).json({message: '이미 등록된 이메일입니다'});
+    }
 
-    return res.status(200).json({ message: '성공', userId: result.userId });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const date = new Date(+new Date() + 3240 * 10000).toISOString().split("T")[0];
+
+    // 관리자 승인 미구현
+    const [result] = await pool.query(
+        'INSERT INTO User (email, password, name, userType, isApproved, createdAt) VALUES (?, ?, ?, ?, ?, ?)',
+        [email, hashedPassword, name, userType, true, date]
+    );
+
+    return res.status(200).json({message: '성공', userId: result.userId});
   } catch (err) {
     console.error(err);
     return res.status(500).json({ message: '서버 에러' });
