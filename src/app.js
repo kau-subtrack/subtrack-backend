7fcbcc8c9190edf7e170cca28abea8abbde3c842
sanghyeok8473 <billy10953@naver.com>
2025-04-19 13:09:39 +0900

    라우터/컨트롤러 분리 및 기능별 API 기본 뼈대 모듈화 구축 (홈/배송/포인트/마이페이지)

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import authRoutes from './routes/auth.js'
+import ownerRoutes from './routes/owner.js';
 
 
 
@@ -13,5 +14,6 @@ app.get('/', (req, res) => {
   res.send('Hello, MongoDB Backend!');
 });
 app.use('/auth', authRoutes);
+app.use('/owner', ownerRoutes);
 
 export default app;
