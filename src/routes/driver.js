5778458980846442f15c67afb4facbded37b00b0
sanghyeok8473 <billy10953@naver.com>
2025-05-18 11:42:14 +0900

    [FEAT] 기사 수거 목록 조회 및 수거 완료 처리 기능 구현

--- src/routes/driver.js
+++ src/routes/driver.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getHomeInfo, getPickUpList, patchPickUp, getDeliveryList, patchDelivery, /*getRouteInfo, postRouteRequest*/ } from "../controllers/driverControllers.js";
+import { getHomeInfo, getPickupList, completePickup, getDeliveryList, patchDelivery, /*getRouteInfo, postRouteRequest*/ } from "../controllers/driverControllers.js";
 import { jwtMiddleware } from '../middlewares/jwtMiddleware.js';  // 필요 시 추가
 
 const router = express.Router();
@@ -10,12 +10,12 @@ router.use(jwtMiddleware);  // 전체 인증 필요 시 사용
 router.get('/home', jwtMiddleware, getHomeInfo); // 홈 화면 정보
 
 // 수거
-router.get('/pick-up', getPickUpList); // 수거 정보 조회
-router.patch('/pick-up/:storeId', patchPickUp); // 수거 정보 변경 요청
+router.get('/pick-up', jwtMiddleware, getPickupList); // 수거 정보 조회
+router.patch('/pick-up/completed', jwtMiddleware, completePickup); // 수거 정보 변경 요청
 
 // 배송
 router.get('/delivery', getDeliveryList); // 배송 정보 조회
-router.patch('/delivery/:trackingCode', patchDelivery); // 배송 정보 변경 요청
+router.patch('/delivery/completed', patchDelivery); // 배송 정보 변경 요청
 
 // 최적경로안내
 // router.get('/route', getCompletedShipments); // 최적 경로 조회
