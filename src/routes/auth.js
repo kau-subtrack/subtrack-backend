796b83925223baa41edea3d3df9ff1b38b5e2a61
nenio15 <2857yoon@gmail.com>
2025-06-04 15:18:42 +0900

    [FEAT] 헬스체크 재확인

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { signup } from '../controllers/signup.js';
 import { login } from '../controllers/login.js';
+import {health} from "../controllers/health.js";
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
+router.get('/health', health);
 
 export default router;
